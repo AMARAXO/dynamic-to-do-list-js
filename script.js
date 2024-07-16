3d6727661a405eb33cabf9a32505a49dfe1fc7a2
Amara <nwokochajanefrances@gmail.com>
2024-07-16 00:24:08 -0500

    updated class list

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function (){
             
             let removeButton = document.createElement('button');
             removeButton.textContent = 'Remove';
-            removeButton.className = 'remove-btn';
+            removeButton.classList.add('remove-btn');
 
             removeButton.onclick = function() {
                 li.remove();
