5e1c6b5115f81fda4b4155efe459fbf2976158f3
Amara <nwokochajanefrances@gmail.com>
2024-07-16 00:15:56 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const addButton = document.getElementById('add-task-btn')
+    const taskInput = document.getElementById('task-input')
+    const taskList = document.getElementById('task-list')
+
+    function addTask (){
+        let taskText = taskInput.value.trim()
+        if (taskText === ''){
+            alert('Please enter a task')
+        }else{
+            let li = document.createElement('li')
+            li.textContent = taskText
+            
+            let removeButton = document.createElement('button');
+            removeButton.textContent = 'Remove';
+            removeButton.className = 'remove-btn';
+
+            removeButton.onclick = function() {
+                li.remove();
+            };
+            li.appendChild(removeButton);
+            taskList.appendChild(li);
+            taskInput.value = '';
+        }
+    }
+    addButton.addEventListener('click', addTask);
+
+    taskInput.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    });
+    addTask();
+});
